466a31275918ada2b177a7f0bc84e73111cbc4af
Kate <ekaterina_borisenko_96@outlook.com>
2021-03-21 13:07:33 +0300

    refactoring for task 2.1.1

--- Task_2/Custom_String_task_2.1.1/Custom_String_task_2.1.1/CusString.cs
+++ Task_2/Custom_String_task_2.1.1/Custom_String_task_2.1.1/CusString.cs
@@ -1,24 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Custom_String_task_2._1._1
 {
-    class CusString : ICusString
+    class CusString : ICusString, IEnumerable<char>, IEnumerator<char>, IEnumerator
     {
         private char[] _array;
+        int _position = -1;
 
         public CusString()
         {
         }
 
-        public CusString(char[] array)                                    // конструктор который конвертирует из массива символов 
+        /// <summary>
+        /// конструктор который конвертирует из массива символов
+        /// </summary>
+        /// <param name="array"></param>
+        public CusString(char[] array)                                    
         {
-            
             _array = array;
         }
 
-        public bool Compare(ICusString cusString)                       // (реализация интерфейса /выполнили контракт ) метод сравнения
+        #region EnumeratorProps
+        public object Current => _array[_position];
+
+        char IEnumerator<char>.Current => _array[_position];
+        #endregion
+
+        #region String
+        /// <summary>
+        /// (реализация интерфейса /выполнили контракт ) метод сравнения
+        /// </summary>
+        /// <param name="cusString"></param>
+        /// <returns></returns>
+        public bool Compare(ICusString cusString)                       
         {
             //  throw new NotImplementedException();
             char[] array = cusString.ToCharArray();
@@ -38,7 +55,12 @@ namespace Custom_String_task_2._1._1
             return true;
         }
 
-        public ICusString ConCat(ICusString cusString)                      // сложение строк
+        /// <summary>
+        /// сложение строк
+        /// </summary>
+        /// <param name="cusString"></param>
+        /// <returns></returns>
+        public ICusString ConCat(ICusString cusString)                     
         {
             List<char> resArray = new List<char>();
             resArray.AddRange(_array);
@@ -49,7 +71,19 @@ namespace Custom_String_task_2._1._1
             return new CusString(res);
         }
 
-        // Возвращает -1, если нет такого символа
+        /// <summary>
+        /// конвертация в массив символов
+        /// </summary>
+        /// <returns></returns>
+        public char[] ToCharArray()
+        {
+            // throw new NotImplementedException();
+            return _array;
+        }
+
+        /// <summary>
+        ///Возвращает -1, если нет такого символа
+        /// </summary>
         public int IndexOf(char value)
         {
             for (int i = 0; i < _array.Length; i++)
@@ -63,12 +97,77 @@ namespace Custom_String_task_2._1._1
             return -1;
         }
 
-       
-
-        public char[] ToCharArray()                                      // конвертация в массив символов
+        public static CusString operator +(CusString firstString, CusString secondString)
         {
-            // throw new NotImplementedException();
-            return _array;
+            return (CusString)firstString.ConCat(secondString);
+        }
+        
+        public static CusString operator +(CusString firstString, string secondString)
+        {
+            return (CusString)firstString.ConCat(new CusString(secondString.ToCharArray()));
+        }
+
+        public static bool operator ==(CusString firstString, CusString secondString)
+        {
+            return firstString.Equals(secondString);
+        }
+        
+        public static bool operator !=(CusString firstString, CusString secondString)
+        {
+            return !firstString.Equals(secondString);
+        }
+        #endregion
+
+        #region IEnumerableMethods
+        public bool MoveNext()
+        {
+            if (_position < _array.Length-1)
+            {
+                _position++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public IEnumerator<char> GetEnumerator()
+        {
+            return this;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return new string(_array);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is CusString)
+            {
+                return Equals(obj as CusString);
+            }
+
+            return false;
+        }
+        public bool Equals(CusString str)
+        {
+            return Compare(str);
         }
     }
 }
--- Task_2/Custom_String_task_2.1.1/Custom_String_task_2.1.1/Program.cs
+++ Task_2/Custom_String_task_2.1.1/Custom_String_task_2.1.1/Program.cs
@@ -15,6 +15,15 @@ namespace Custom_String_task_2._1._1
             Console.WriteLine(cs3.IndexOf('l'));
             Console.WriteLine(cs1.Compare(cs3));
             Console.WriteLine(cs1.Compare(cs2));
+
+            foreach (var item in cs1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(cs1 + " " + cs2);
+            Console.WriteLine(cs1 == cs3);
+            Console.WriteLine(cs1 != cs2);
         }
     }
 }
