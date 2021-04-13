f2db1bf3e8c697a02462b390f9c273f44ffd6e75
Kate <ekaterina_borisenko_96@outlook.com>
2021-04-14 01:47:10 +0300

    add task 3.3.1

---
+++ Task_3/Task 3.3/Task_3_3/Task_3_3/Extensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3_3
+{
+    static class Extensions
+    {
+        public static void DoSomething(this int[] array, Func<int, int> func)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = func(array[i]);
+            }
+        }
+        
+        public static int GetSumm(this int[] array)
+        {
+            var summ = 0;
+
+            array.ToList().ForEach(i => summ += i);
+
+            return summ;
+        }
+
+        public static double GetAverageValue(this int[] array)
+        {
+            return array.GetSumm() / array.Length;
+        }
+        public static int GetFrequently(this int[] array)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            array.ToList().ForEach(i =>
+            {
+                AddToDictionary(array, i, dictionary);
+            });
+
+            var frequently = 0;
+
+            foreach (var item in dictionary)
+            {
+                if (item.Value > frequently)
+                    frequently = item.Value;
+            }
+
+            return dictionary.Where(dict => dict.Value == frequently).ToList()[0].Key;
+        }
+
+        private static void AddToDictionary(int[] array, int i, Dictionary<int, int> dictionary)
+        {
+            var isAdded = dictionary.TryAdd(i, 1);
+
+            if (!isAdded)
+            {
+                dictionary[i] = dictionary[i]++;
+            }
+        }
+    }
+}
---
+++ Task_3/Task 3.3/Task_3_3/Task_3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] {4, 1,2,3,4,5,6,7,8,1,1,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,3,3 };
+
+            array.DoSomething(i => i * 2);
+            Console.WriteLine($"Summ: {array.GetSumm()}");
+            Console.WriteLine($"Frequently: {array.GetFrequently()}");
+            Console.WriteLine(array.GetAverageValue());
+
+            array.ToList().ForEach(i => Console.WriteLine(i));
+        }
+    }
+}
