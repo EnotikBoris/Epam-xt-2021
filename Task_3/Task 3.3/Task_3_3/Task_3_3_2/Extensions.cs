95ae21efebd6b524634a70a2d5adf1710dc5b301
Kate <ekaterina_borisenko_96@outlook.com>
2021-04-14 16:41:50 +0300

    Task 3.3.2 was added

---
+++ Task_3/Task 3.3/Task_3_3/Task_3_3_2/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task_3_3_2
+{
+    public static class Extensions
+    {
+        public static WordCategory GetWordCategory(this string s)
+        {
+            var regexForRussian = new Regex(@"^[А-Яа-я][А-Яа-я]+[А-Яа-я]$");
+            var regezForEnglish = new Regex(@"^[A-Za-z][A-Za-z]*[A-Za-z]$");
+            var regexForNumbers = new Regex(@"^\d\d*\d$");
+
+            if (regexForRussian.IsMatch(s))
+            {
+                return WordCategory.Ru;
+            }else if (regezForEnglish.IsMatch(s))
+            {
+                return WordCategory.En;
+            }else if (regexForNumbers.IsMatch(s))
+            {
+                return WordCategory.Number;
+            }
+            else
+            {
+                return WordCategory.Mixed;
+            }
+        }
+    }
+
+    public enum WordCategory
+    {
+        Ru = 1,
+        En = 2,
+        Number = 3,
+        Mixed = 4,
+    }
+}
---
+++ Task_3/Task 3.3/Task_3_3/Task_3_3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_3_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test".GetWordCategory());
+            Console.WriteLine("Тест".GetWordCategory());
+            Console.WriteLine("1234789".GetWordCategory());
+            Console.WriteLine("Test1234789".GetWordCategory());
+        }
+    }
+}
